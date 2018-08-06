db656571f871f387d169cd8d81dce4a63650d8d6
Darko Tasevski <c.dtasevski@gogoair.com>
2018-08-06 18:14:59 +0200

    Add PATCH route for /todos

--- server/server.js
+++ server/server.js
@@ -49,6 +49,34 @@ app.delete('/todos/:todo_id', (req, res) => {
 		.catch(err => res.status(400).json(err));
 });
 
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+
+			res.send({ todo });
+		})
+		.catch(e => {
+			res.status(400).send();
+		});
+});
+
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
 
 module.exports = { app };
