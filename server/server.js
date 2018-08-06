ede6afada7625f7ac1f68752cad9f50c9ba5183b
Darko Tasevski <c.dtasevski@gogoair.com>
2018-08-06 15:46:42 +0200

    Add test for /GET /todos

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/todos', (req, res) => {
+	Todo.find()
+		.then(todos => res.send({ todos }))
+		.catch(err => res.status(400).send(err));
+});
+
 app.post('/todos', (req, res) => {
 	const todo = new Todo({
 		text: req.body.text,
