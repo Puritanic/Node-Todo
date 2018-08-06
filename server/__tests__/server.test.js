4cbda7bc0f0561fefcf8ff92f86d21bec6e2a7eb
Darko Tasevski <c.dtasevski@gogoair.com>
2018-08-06 17:42:09 +0200

    Add DELETE route for /todos

--- server/__tests__/server.test.js
+++ server/__tests__/server.test.js
@@ -1,14 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const Todo = require('../models/todo');
 const User = require('../models/user');
 
 const todos = [
-	{ text: ' 1st Test Todo Text' },
-	{ text: ' 2nd Test Todo Text' },
-	{ text: ' 3rd Test Todo Text' },
+	{ text: '1st Test Todo Text', _id: new ObjectID() },
+	{ text: '2nd Test Todo Text', _id: new ObjectID() },
+	{ text: '3rd Test Todo Text', _id: new ObjectID() },
 ];
 
 beforeEach(done => {
@@ -70,6 +71,35 @@ describe('GET /todos', () => {
 			.get('/todos')
 			.expect(200)
 			.expect(res => expect(res.body.todos.length).toBe(3))
+			.end(done);
+	});
+});
+
+describe('GET /todos/:todo_id', () => {
+	it('should get todo by id', done => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect(res => expect(res.body.todo.text).toBe(todos[0].text))
+			.end(done);
+	});
+});
+
+describe('DELETE /todos/:todos_id', () => {
+	const id = new ObjectID().toHexString();
+
+	it('should remove a todo', done => {
+		request(app)
+			.delete(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect(res => expect(res.body.text).toBe(todos[0].text))
+			.end(done);
+	});
+
+	it('should return 404 if todo not found', done => {
+		request(app)
+			.delete(`/todos/${id}`)
+			.expect(404)
 			.end(done);
 	});
 });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const Todo = require('./models/todo');
@@ -12,8 +13,14 @@ app.use(bodyParser.json());
 
 app.get('/todos', (req, res) => {
 	Todo.find()
-		.then(todos => res.send({ todos }))
+		.then(todos => res.json({ todos }))
 		.catch(err => res.status(400).send(err));
+});
+
+app.get('/todos/:todo_id', (req, res) => {
+	return Todo.findById(req.params.todo_id)
+		.then(todo => res.json({ todo }))
+		.catch(err => res.status(400).json(err));
 });
 
 app.post('/todos', (req, res) => {
@@ -23,9 +30,23 @@ app.post('/todos', (req, res) => {
 
 	todo.save()
 		.then(todo => {
-			res.send(todo);
+			res.json(todo);
 		})
 		.catch(err => res.status(400).send(err));
+});
+
+app.delete('/todos/:todo_id', (req, res) => {
+	const id = req.params.todo_id;
+
+	if (!ObjectID.isValid(id)) return res.status(404).send();
+
+	return Todo.findByIdAndDelete(id)
+		.then(response => {
+			if (!response) return res.status(404).send();
+
+			return res.status(200).json(response);
+		})
+		.catch(err => res.status(400).json(err));
 });
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
